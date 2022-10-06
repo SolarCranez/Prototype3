dc2515757135adcfcd5313f02fa7c2cff9adfc9c
SolarCranez <fragglekids2@gmail.com>
2022-10-06 09:01:50 -0500

    finished prototype3

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
-    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private Vector3 spawnPos = new Vector3(25, 0.001f, 0);
     public PlayerController playerControllerScript;
 
     private int startDelay = 2;
